b56ff3408f8bdce6c578beaa8c16286ff9c31302
Sreenidhi <sreenidhimuralidharan@gmail.com>
2014-10-11 01:11:05 -0700

    Commit of NewsViewController- final

--- sources/NewsViewController.m
+++ sources/NewsViewController.m
@@ -14,6 +14,10 @@
 
 static NSString *const kNewsCellIdentifier = @"NewsCell";
 
+/*
+ * This menu item loads the news from an array of news and displays them as a table.
+ * Each cell of the table contains a thumbnail image and a short description of what the image is about (news).
+ */
 
 @implementation NewsViewController
 
@@ -27,35 +31,48 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 {
     [super didReceiveMemoryWarning];
 }
-
+/*
+ *  Function for loading app screen.
+ * The screen is reloaded by a single data which doesn't vary or there is no editing operation
+ * performed on data
+ * @return Nothing
+ */
 - (void) viewDidLoad
 {
+    //Sets the view when the screen for News Loads
     [super viewDidLoad];
     
+    //Sets the menu bar animation. Enables the user to switch between menu tabs.
 	tabBarAnimation = YES;
 	
 	titleFont = [UIFont systemFontOfSize:[UIFont labelFontSize]];
-
+    
 	NSDictionary *attribute = [NSDictionary dictionaryWithObject:[UIFont boldSystemFontOfSize:16.0f] forKey:NSFontAttributeName];
 	[switchTitle setTitleTextAttributes:attribute forState:UIControlStateNormal];
 	[switchTitle removeSegmentAtIndex:1 animated:NO];
-
+    
 	refreshControl = [UIRefreshControl new];
 	// refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Updating News..."];
 	[refreshControl addTarget:self action:@selector(refresh) forControlEvents:UIControlEventValueChanged];
 	[((UITableViewController*)self.newsTableView.delegate) setRefreshControl:refreshControl];
 	[self.newsTableView addSubview:refreshControl];
-
+    
+    // Sets the header and footer descriptions for the screen
 	newsTableView.tableHeaderView = nil;
 	newsTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
 }
+
+/*
+ * Function for loading the screen with new data every time
+ * @return Nothing
+ */
 
 - (void) viewWillAppear:(BOOL)animated
 {
 	[super viewWillAppear: animated];
 	
 	[self performSelectorOnMainThread:@selector(loadData) withObject:nil waitUntilDone:NO];
-
+    
 	if(tabBarAnimation)
 	{
 		[appDelegate.tabBar.view setHidden:YES];
@@ -63,7 +80,10 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receivedNotification:) name:FEED_UPDATED_NOTIFICATION object:nil];
 }
-
+/*
+ * Function to be called every time this view ceases to be the frontmost view
+ * @return Nothing
+ */
 - (void) viewWillDisappear:(BOOL)animated
 {
 	[super viewWillDisappear: animated];
@@ -71,25 +91,30 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+/*
+ * Function to be called when the view is actually visible
+ * @return Nothing
+ */
+
 - (void) viewDidAppear:(BOOL)animated
 {
 	[super viewDidAppear: animated];
-
+    
 	if(tabBarAnimation)
 	{
 		// Don't show an ugly jerk while the bottom tabbar is drawn
 		[UIView transitionWithView:appDelegate.tabBar.view duration:0.4 options:UIViewAnimationOptionTransitionCrossDissolve
-		animations:^
-		{
-			[appDelegate.tabBar.view setHidden:NO];
-		}
-		completion:nil];
+                        animations:^
+         {
+             [appDelegate.tabBar.view setHidden:NO];
+         }
+                        completion:nil];
 		
 		tabBarAnimation = NO;
 	}
 	
 	if([[NSUserDefaults standardUserDefaults] boolForKey:@"NewsUpdated"])
-	{
+	{   // If the current news is updated, display News Updated message
 		[appDelegate showMessage:@"News have been updated" onView:self.view hideAfter:3.0];
 		
 		[[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"NewsUpdated"];
@@ -98,29 +123,40 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 }
 
 #pragma mark - Private Methods
-
+/*
+ * Function to refresh the view
+ * @return Nothing
+ */
 - (void) refresh
 {
 	[self loadData];
-		
+    
 	[refreshControl endRefreshing];
 	
 	[NSThread sleepForTimeInterval:0.5];
 }
 
+/*
+ * Function to give notification on news update
+ * @return Nothing
+ */
 - (void) receivedNotification:(NSNotification*) notification
 {
     if ([[notification name] isEqualToString:FEED_UPDATED_NOTIFICATION]) // Not really necessary until there is only one notification
 	{
  		[self performSelectorOnMainThread:@selector(loadData) withObject:nil waitUntilDone:NO];
-
+        
 		[appDelegate showMessage:@"News have been updated" onView:self.view hideAfter:3.0];
-
+        // Set User Defaults to persist data across app launches
 		[[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"NewsUpdated"];
         [[NSUserDefaults standardUserDefaults] synchronize];
 	}
 }
 
+/*
+ * Function to load the News data
+ * @return Nothing
+ */
 - (void) loadData
 {
 	news = [NSMutableArray new];
@@ -131,11 +167,12 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 	myString = [myString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
 	myString = [myString stringByReplacingOccurrencesOfString:@"\t" withString:@""];
 	xmlData = [myString dataUsingEncoding:NSUTF8StringEncoding];
-	
+	// News is represented as an XML Document
 	DDXMLDocument *newsXMLDoc = [[DDXMLDocument alloc] initWithData:xmlData options:0 error:nil];
 	DDXMLElement *rootElement = [newsXMLDoc rootElement];
-
+    
 	NSInteger nodeCount = [rootElement childCount];
+    // Loop through the news array
 	for (NSInteger nodeIdx = 0; nodeIdx < nodeCount; nodeIdx++)
 	{
 		DDXMLElement *child = (DDXMLElement*)[rootElement childAtIndex:nodeIdx];
@@ -147,13 +184,13 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 			for (NSInteger subNodeIdx = 0; subNodeIdx < subNodeCount; subNodeIdx++)
 			{
 				DDXMLElement *newsNode = (DDXMLElement*)[child childAtIndex:subNodeIdx];
-				
+				// Get the name, desciption, event image, info and image
 				NSString *name = @"";
 				NSString *description = @"";
 				NSString *eventImageUrl = @"";
 				NSString *info = @"";
 				NSString *thumbImageUrl = @"";
-							
+                
 				NSInteger subNode2Count = [newsNode childCount];
 				if(subNode2Count != 0)
 				{
@@ -161,7 +198,7 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 					{
 						DDXMLElement *newsSubNode = (DDXMLElement*)[newsNode childAtIndex:subNodeIdx];
 						NSString *subNodename = [newsSubNode name];
-						
+						//Set each heading as follows
 						if ([subNodename isEqualToString:@"Name"])
 						{
 							name = [newsSubNode stringValue];
@@ -183,7 +220,7 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 							thumbImageUrl = [newsSubNode stringValue];
 						}
 					}
-				
+                    // Add these to a NSMutableDictionary of items
 					NSMutableDictionary *newsItem = [NSMutableDictionary new];
 					[newsItem setObject:name forKey:@"name"];
 					[newsItem setObject:description forKey:@"description"];
@@ -196,7 +233,7 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 			}
 		}
 	}
-
+    
 	[self.newsTableView reloadData];
 }
 
@@ -206,12 +243,18 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 {
     return 1;
 }
-
+/*
+ * Function to draw the table view
+ * @return Returns the number of news items available
+ */
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 	return [news count];
 }
-
+/*
+ * Function to place each news item in each cell of the table
+ * @return Returns each table cell
+ */
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	NSUInteger row = [indexPath row];
@@ -228,6 +271,7 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 	}
 	
 	CGSize imgSize = CGSizeMake(0.0, 0.0);
+    //Sets the thumbnail image
 	NSString *imageUrl = [newsData objectForKey:@"thumbImage"];
 	if(imageUrl.length != 0)
 	{
@@ -243,22 +287,23 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 			[cell.contentView addSubview:imageView];
 		}
 	}
-	
+	// Sets the title for each cell
 	UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(15.0, 4.0 + imgSize.height, 305.0, 48.0)];
 	titleLabel.tag = CELL_TITLE_LABEL_TAG;
 	titleLabel.font = titleFont;
 	titleLabel.numberOfLines = 2;
 	titleLabel.text = [newsData objectForKey:@"name"];
-
+    
 	CGSize size = [titleLabel.text sizeWithAttributes:@{ NSFontAttributeName : titleFont }];
 	if(size.width < 285.0)
 	{
+        // The dimensions of the cell that holds the news
 		[titleLabel setFrame:CGRectMake(15.0, 4.0 + imgSize.height, 305.0, 26.0)];
 		titleLabel.numberOfLines = 1;
 	}
 	
 	[cell.contentView addSubview:titleLabel];
-
+    
 	// cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 	cell.selectionStyle = UITableViewCellSelectionStyleNone;
 	
@@ -266,30 +311,38 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 }
 
 #pragma mark - UITableView Delegate
-
+/*
+ * Function to perform some action when the user taps on a cell
+ * @return Nothing
+ */
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	NSUInteger row = [indexPath row];
 	NSDictionary *newsData = [news objectAtIndex:row];
-
+    
 	NewsDetailViewController *eventDetail = [[NewsDetailViewController alloc] initWithNews:newsData];
 	[self.navigationController pushViewController:eventDetail animated:YES];
 	
 	[tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+/*
+ * Function to set the cell's height accordingly
+ */
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    //Creates a news array
 	NSMutableDictionary *newsData = [news objectAtIndex:[indexPath row]];
-
+    
 	CGFloat height = 54.0;
+    // After setting cell height, display title and image
 	NSString *text = [newsData objectForKey:@"name"];
 	CGSize size = [text sizeWithAttributes:@{ NSFontAttributeName : titleFont }];
 	if(size.width < 285.0)
 	{
 		height = 34.0;
 	}
-
+    
 	NSString *imageUrl = [newsData objectForKey:@"thumbImage"];
 	if(imageUrl.length != 0)
 	{
@@ -303,7 +356,7 @@ static NSString *const kNewsCellIdentifier = @"NewsCell";
 			}
 		}
 	}
-
+    
 	return height;
 }
 
